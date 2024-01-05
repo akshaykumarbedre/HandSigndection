4698f3f3aa4d7d7d06e9cdaaa8762cff6f81280a
akshaykumarbedre <90820098+akshaykumarbedre@users.noreply.github.com>
2024-01-05 19:08:08 +0530

    improved result

--- collect_imgs.py
+++ collect_imgs.py
@@ -8,7 +8,7 @@ if not os.path.exists(DATA_DIR):
 number_of_classes = 5
 dataset_size = 100
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
--- inference_classifier.py
+++ inference_classifier.py
@@ -8,14 +8,14 @@ import time
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'Yoho', 1: 'Peace', 2: 'Like',3:"Shoot",4:"Cool",}
+labels_dict = {0: 'Peace', 1: 'Yoho', 2: 'Like',3:"Cool",4:"Shoot",}
 while True:
 
     data_aux = []
